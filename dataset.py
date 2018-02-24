8da8d045a90f399b8ae47e9bb12274cc4530fa1f
Pushpankar <pushpankark@gmail.com>
2018-02-24 15:01:23 +0530

    Add dataloader

---
+++ dataset.py
@@ -0,0 +1,28 @@
+import torch
+import pandas as pd
+from torch.utils.data import Dataset
+from random import randint
+
+class JobDataset(Dataset):
+	def __init__(self, csv_file):
+		self.data = pd.read_csv(csv_file)
+
+	def __len__(self):
+		return self.data.shape[0]
+
+	def __getitem__(self, idx):
+		r = randint(1,10)
+
+		sample = {
+			"description": self.data[2][idx]
+		}
+		if r > 5:
+			sample["title"] = self.data[1][idx]
+			sample["related"] = 1
+		else:
+			true_title = self.data[1][idx]
+			false_label = self.data[self.data[1] != true_title][1].sample()
+			sample["title"] = false_label
+			sample["related"] = -1
+
+		return sample
